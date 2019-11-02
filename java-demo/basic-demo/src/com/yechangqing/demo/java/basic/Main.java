7ec6c55f5cdc6309a3b556911b6523e142fffde1
austinyee <yecq1998@outlook.com>
2019-11-02 22:39:43 +0800

    Remove c-demo

---
+++ java-demo/basic-demo/src/com/yechangqing/demo/java/basic/Main.java
@@ -0,0 +1,24 @@
+package com.yechangqing.demo.java.basic;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        List array = new ArrayList<>(20);
+        for(int i=0;i<15;i++){
+          array.add(i);
+        }
+
+        AtomicInteger x = new AtomicInteger(0);
+        array.parallelStream().forEach(it -> {
+            System.out.println(it);
+            x.getAndIncrement();
+        });
+
+        System.out.println(x.intValue());
+    }
+}
