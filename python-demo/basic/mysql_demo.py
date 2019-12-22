579bd0b30009d9d93762952ea5dd6977a5329961
austinyee <yecq1998@outlook.com>
2019-12-22 20:52:12 +0800

    :feat: add python mysql demo :bug: Fixing a bug :fire: Removing code or files. :zap: Improving performance. :art: Improving structure / format of the code. :sparkles: Introducing new features. :ambulance: Critical hotfix. :pencil: Writing docs. :white_check_mark: Updating tests. :pushpin: Pinning dependencies to specific versions. :arrow_up: Upgrading dependencies. :green_heart: Fixing CI Build. :bookmark: Releasing <Version> tags. :rotating_light: Removing linter warnings. :recycle: Refactoring code. :wrench: Changing configuration files. :heavy_plus_sign: Adding a dependency. :heavy_minus_sign: Removing a dependency. :truck: Moving or renaming files :alien: Updating code due to external API changes.

--- python-demo/basic/mysql_demo.py
+++ python-demo/basic/mysql_demo.py
@@ -1,16 +1,16 @@
 import pymysql.cursors
 
 if __name__ == '__main__':
-    conn = pymysql.connect(host='localhost',
+    conn = pymysql.connect(host='yechangqing.com',
                            user='root',
-                           password='gllue123',
+                           password='Ycq980518',
                            db='test_db')
     try:
         with conn.cursor() as cursor:
             sql = "select * from `user`"
-
-
-
+            cursor.execute(sql)
+            result = cursor.fetchone()
+            print(result)
     finally:
         conn.close()
 
