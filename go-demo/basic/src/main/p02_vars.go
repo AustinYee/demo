70b1e6d130c00e7288503958e4110c57040d4d1f
changqingyeh <yecq1998@outlook.com>
2019-12-27 10:21:17 +0800

    :fire: remove go demo

--- go-demo/basic/src/main/p02_vars.go
+++
@@ -1,9 +0,0 @@
-package main
-
-const TEST_CONST  = 100
-
-func main() {
-	var str string = "web world"
-	var a,b = 3,5
-	println(str, a, b, TEST_CONST)
-}
--- go-demo/web/src/dao/main.go
+++
@@ -1,9 +0,0 @@
-package dao
-
-import "database/sql"
-
-func init() {
-	db, _ := sql.Open("mysql",  "root:gllue123@127.0.0.1:3306/test");
-	stmt, _ := db.Prepare("insert into test (name) values ('?')");
-	stmt.Exec("test");
-}
