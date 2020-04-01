889e555bcf517d47b0ef9098167d8be5e8582b6a
changqingyeh <yecq1998@outlook.com>
2020-04-01 09:01:30 +0800

    BigDecimalDemo

--- java-demo/springboot-demo/springboot-redis/src/test/java/com/yechangqing/demo/java/springboot/redis/ApplicationTest.java
+++ java-demo/springboot-demo/springboot-redis/src/test/java/com/yechangqing/demo/java/springboot/redis/ApplicationTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @SpringBootTest(classes = Application.class)
@@ -13,8 +14,17 @@ public class ApplicationTest {
 
   @Autowired private StudentService studentService;
 
+  @Autowired
+  private RedisTemplate<String, String> redisTemplate;
+
   @Test
   public void test() {
     studentService.test("test", 11);
+  }
+
+  @Test
+  public  void test2(){
+    var result = redisTemplate.opsForValue().increment("test3", 0.00001);
+    System.out.println(result);
   }
 }
