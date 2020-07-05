6b2a5005ffc7b606317838c89d42163f8c8c0e29
changqingyeh <yecq1998@outlook.com>
2020-07-05 23:38:31 +0800

    Demo

--- go-demo/go-basic/demo10-mysql.go
+++ go-demo/go-basic/demo10-mysql.go
@@ -1,10 +1,43 @@
 package main
 
 import (
-	_ "com.github/go-sql-driver/mysql"
+	"database/sql"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
 )
 
+type User struct {
+	id   int
+	name string
+}
+
+type result struct {
+	code int
+	messge string
+	data interface{}
+}
+
 func main() {
-	db, err := mysql.Open("mysql", "root:gllue123@tcp(localhost:3306)/test")
-	db.
+	db, err := sql.Open("mysql", "root:ycq980518@tcp(localhost:3306)/test")
+	if err != nil {
+		fmt.Print("Error:%v", err)
+	}
+	_ , err = db.Exec("insert into test (name) values (?)", "changqing")
+	if err != nil {
+		fmt.Print("Error:%v", err)
+	}
+
+	rows, err := db.Query("select * from test")
+	user := new(User)
+	user_map := make(map[int]string)
+	for rows.Next() {
+		rows.Scan(&user.id, &user.name)
+		fmt.Println(*user)
+		user_map[user.id] = user.name
+	}
+	result := new(result)
+	result.code = 200
+	result.messge = "success"
+	result.data = user_map
+	fmt.Print(*result)
 }
--- go-demo/go-basic/go.mod
+++ go-demo/go-basic/go.mod
@@ -1,3 +1,8 @@
 module go-basic
 
 go 1.13
+
+require (
+	github.com/aiscrm/redisgo v0.2.1
+	github.com/go-sql-driver/mysql v1.5.0
+)
