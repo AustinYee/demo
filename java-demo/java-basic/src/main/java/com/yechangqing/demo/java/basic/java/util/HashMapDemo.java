50b42c35f0c7e7abf561dc25c1ce3b5d59e57883
changqingyeh <yecq1998@outlook.com>
2020-07-30 23:42:22 +0800

    Add hashmap demo

--- java-demo/java-basic/src/main/java/com/yechangqing/demo/java/basic/java/util/HashMapDemo.java
+++ java-demo/java-basic/src/main/java/com/yechangqing/demo/java/basic/java/util/HashMapDemo.java
@@ -1,11 +1,9 @@
-package com.yechangqing.demo.java.basic.java.util;
-
 import java.util.HashMap;
 
 public class HashMapDemo {
   public static void main(String[] args) {
     var map = new HashMap<String, String>();
     map.put("test", "test");
-    System.out.println(map.get("test"));
-  }
+    map.put("test", "test");
+    System.out.println(map.get("test"));}
 }
